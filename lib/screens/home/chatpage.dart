e8962e49034d7c8c57cd0d4c826181bd4a83da23
mohanrajreese <mohanrj1345@gmail.com>
2021-10-22 10:35:25 +0530

    Bottom navbar completed

--- lib/screens/home/chatpage.dart
+++ lib/screens/home/chatpage.dart
@@ -7,8 +7,6 @@ import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
 import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:xenchat/screens/home/profilepage.dart';
-import 'lib.dart';
-
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -35,7 +33,49 @@ class _ChatPageState extends State<ChatPage> {
             ),
             centerTitle: true,
           ),
-          bottomNavigationBar: FloatingNavbar(
+          // bottomNavigationBar: FloatingNavbar(
+          //   onTap: (int val) {
+          //     setState(() { _index = val; } );
+          //     if(val==0){
+          //       Navigator.push(context,
+          //           MaterialPageRoute(
+          //             builder: (context)=> ChatPage(),
+          //           )
+          //       );
+          //     }
+          //     if(val==1){
+          //       Navigator.push(context,
+          //           MaterialPageRoute(
+          //             builder: (context)=> Contact(),
+          //           )
+          //       );
+          //     }
+          //     if(val==2){
+          //       Navigator.push(context,
+          //           MaterialPageRoute(
+          //             builder: (context)=> ProfilePage(),
+          //           )
+          //       );
+          //     }
+          //     if(val==3){
+          //       Navigator.push(context,
+          //           MaterialPageRoute(
+          //             builder: (context)=> landingPage(),
+          //           )
+          //       );
+          //     }
+          //   },
+          //   backgroundColor: CupertinoColors.systemPink,
+          //   currentIndex: _index,
+            // items: [
+            //   FloatingNavbarItem(icon: Icons.message_outlined),
+            //   FloatingNavbarItem(icon: Icons.contact_page_outlined ),
+            //   FloatingNavbarItem(icon: Icons.account_box_outlined),
+            //   FloatingNavbarItem(icon: Icons.exit_to_app_outlined),
+            // ],
+          // ),
+
+          bottomNavigationBar: BottomNavigationBar(
             onTap: (int val) {
               setState(() { _index = val; } );
               if(val==0){
@@ -69,13 +109,33 @@ class _ChatPageState extends State<ChatPage> {
             },
             backgroundColor: CupertinoColors.systemPink,
             currentIndex: _index,
-            items: [
-              FloatingNavbarItem(icon: Icons.message_outlined),
-              FloatingNavbarItem(icon: Icons.contact_page_outlined ),
-              FloatingNavbarItem(icon: Icons.account_box_outlined),
-              FloatingNavbarItem(icon: Icons.exit_to_app_outlined),
-            ],
-          ),
+          
+          items: const <BottomNavigationBarItem>[
+            
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_sharp,size: 40.0, color: Colors.cyan,),
+              label: 'Messages',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.contacts_rounded,
+                size: 40.0,
+                color: Colors.cyan,
+              ),
+              label: 'Contacts',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline_sharp,size: 40.0, color: Colors.cyan ,),
+              label: 'Profile',
+              
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.logout,size: 30.0, color: Colors.cyan,),
+              label: 'LogOut',              
+            ),
+          ],
+          ), 
+
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -86,7 +146,7 @@ class _ChatPageState extends State<ChatPage> {
                   child: Text(
                     'LOGOUT',
                     style: TextStyle(
-                        fontSize: 20, color: Colors.white, letterSpacing: 1.0),
+                        fontSize: 20, color: Colors.cyanAccent, letterSpacing: 1.0),
                   ),
                   onPressed: () async {
                     await FirebaseAuth.instance.signOut();
