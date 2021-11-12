701fe249dc15371e12922814ed9b70e7c35487de
mohanrajreese <mohanrj1345@gmail.com>
2021-11-12 13:41:35 +0530

    One to One Chat added

---
+++ lib/Authenticate/LoginScree.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+import 'package:xenchat/Authenticate/CreateAccount.dart';
+import 'package:xenchat/Authenticate/Methods.dart';
+import 'package:xenchat/Screens/HomeScreen.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: isLoading
+          ? Center(
+              child: Container(
+                height: size.height / 20,
+                width: size.height / 20,
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: size.height / 20,
+                  ),
+                  // Container(
+                  //   alignment: Alignment.centerLeft,
+                  //   width: size.width / 0.5,
+                  //   child: IconButton(
+                  //       icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
+                  // ),
+                  Image.asset(
+                  'media/XenLogo.png',
+                  height: 150.0,
+                  width: 600.0,
+                ),
+                // Spacer(
+                //   flex:3,
+                //   ),
+              
+                  SizedBox(
+                    height: size.height / 50,
+                  ),
+                  Container(
+                    width: size.width / 1.1,
+                    child: Text(
+                      "Sign In To Continue",
+                      style: TextStyle(
+                        fontFamily: 'Rubik',
+                      fontSize: 30,
+                      color: Colors.pinkAccent,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 0.8),
+                  textAlign: TextAlign.center,
+                    ),
+                  ),
+                 
+                  SizedBox(
+                    height: size.height / 12,
+                  ),
+                  
+                  Container(
+
+                    width: size.width,
+                    alignment: Alignment.center,
+                    child: field(size, "email",  Icons.account_box, _email),
+                  ),
+                  Padding(
+                    padding: const 
+                    EdgeInsets.symmetric(
+                      vertical: 18.0
+                    ),
+                    child: Container(
+                      width: size.width,
+                      alignment: Alignment.center,
+                      child: field(size, "password", Icons.lock, _password),
+                    ),
+                  
+                  ),
+                  SizedBox(
+                    height: size.height / 10,
+                  ),
+                  customButton(size),
+                  SizedBox(
+                    height: size.height / 40,
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (_) => CreateAccount())),
+                    child: Text(
+                      "Create Account",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+    );
+  }
+
+  Widget customButton(Size size) {
+    return GestureDetector(
+      onTap: () {
+        if (_email.text.isNotEmpty && _password.text.isNotEmpty) {
+          setState(() {
+            isLoading = true;
+          });
+
+          logIn(_email.text, _password.text).then((user) {
+            if (user != null) {
+              print("Login Sucessfull");
+              setState(() {
+                isLoading = false;
+              });
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => HomeScreen()));
+            } else {
+              print("Login Failed");
+              setState(() {
+                isLoading = false;
+              });
+            }
+          });
+        } else {
+          print("Please fill form correctly");
+        }
+      },
+      child: Container(
+          height: size.height / 14,
+          width: size.width / 1.2,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.pinkAccent,
+          ),
+          alignment: Alignment.center,
+          child: Text(
+            "Login",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+    );
+  }
+
+  Widget field(
+      Size size, String hintText, IconData icon, TextEditingController cont) {
+    return Container(
+      height: size.height / 14,
+      width: size.width / 1.1,
+      child: TextField(
+        controller: cont,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon),
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.blue),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,19 @@
-import 'dart:async';
-
-import 'package:xenchat/screens/Authentication/login.dart';
-import 'package:xenchat/screens/home/profilepage.dart';
-import 'package:xenchat/screens/home/contact.dart';
-import 'package:xenchat/screens/home/chatpage.dart';
-import 'package:xenchat/shared/loading.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:xenchat/shared/loading.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import 'package:xenchat/Authenticate/Autheticate.dart';
 import 'package:firebase_core/firebase_core.dart';
-import './screens/Authentication/login.dart';
-import './screens/Authentication/signup.dart';
-import 'landingPage.dart';
+import 'package:flutter/material.dart';
 
-Future<void> main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-  dynamic currentUserData =
-      FirebaseAuth.instance.authStateChanges().listen((User? user) {
-    if (user == null) {
-      print("user is currently not logged in");
-      runApp(MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: landingPage(),
-        routes: {
-          '/landingpage': (context) => landingPage(),
-          '/signup': (context) => Signup(),
-          '/login': (context) => Login(),
-          '/loading': (context) => Loading(),
-          '/profilepage': (context) => ProfilePage(),
-          '/chatpage': (context) => ChatPage(),
-          '/contact': (context) => Contact()
-        },
-      ));
-    } else {
-      print("user = $user");
-      runApp(ChatPage());
-    }
-  });
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Authenticate(),
+    );
+  }
+}
